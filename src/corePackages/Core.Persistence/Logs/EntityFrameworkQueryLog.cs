acabc1bbd49ce113ef8a61a89cba62dc273be2d3
Emre Can Ayar <emrecan.ayar@hotmail.com>
2023-08-07 20:09:02 +0300

    Create EntityFrameworkQueryLog.cs

---
+++ src/corePackages/Core.Persistence/Logs/EntityFrameworkQueryLog.cs
@@ -0,0 +1,14 @@
+﻿namespace Core.Persistence.Logs
+{
+    public class EntityFrameworkQueryLog
+    {
+        public static void LogQuery(string query)
+        {
+            string currentPath = $@"{Directory.GetCurrentDirectory()}\Logs\EntityFrameworkQueryLogs\Logs.txt";
+            using (StreamWriter writer = new StreamWriter(currentPath, true))
+            {
+                writer.WriteLine(query);
+            }
+        }
+    }
+}
