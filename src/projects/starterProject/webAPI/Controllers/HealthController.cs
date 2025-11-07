8e7837a57cbc3dc6f7662e674dbc8697cc12cc16
Ali ŞİMŞEK <ali.simsek@technoface.com.tr>
2025-11-07 14:00:35 +0300

    add heaalth controller

---
+++ src/projects/starterProject/webAPI/Controllers/HealthController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace webAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                status = "Healthy",
+                timestamp = DateTime.UtcNow,
+            });
+        }
+    }
+}
